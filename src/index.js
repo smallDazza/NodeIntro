f8bf29a4c381bd9b9477aa35b64106794769c28a
smallDazza <smalldazza@gmail.com>
2024-10-24 16:22:22 +1100

    docs: added nodemon and js docs packages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("Dice Roller Application starts...")
+const inquirer = require("inquirer").default;
+
+console.log("Dice Roller Application starts...");
+/**
+ * Roll a random dice side from a rolling of a dice.
+ * @param {number} diceSide The total number of sides on the dice
+ * @returns The side of the dice rolled.
+ */
+function rollDice(diceSide=6) {
+    let diceResult = 0;
+    diceResult = Math.ceil(Math.random() * diceSide);
+    return diceResult;
+}
+
+//console.log("The dice side you rolled is: " + rollDice());
+
+const prompt = inquirer.createPromptModule();
+
+prompt([
+    {
+        type: "number",
+        name: "dice_side",
+        message: "How many sided dice do you want to roll ?"
+    }
+]).then((answer) => {
+    console.log(rollDice(answer.dice_side))
+});
+
